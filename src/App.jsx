8f93381f81c2997964a5e1f274d805b1d4d9a635
Ashique Ali <ashiqueali@ikm.gov.in>
2025-06-15 08:29:00 +0530

    Leabel chage

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and savess to test HMR
+          Edit <code>src/App.jsx</code> and savesss to test HMR
         </p>
       </div>
       <p className="read-the-docs">
